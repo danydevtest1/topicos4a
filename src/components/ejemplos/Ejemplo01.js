2d406437d5f1954c122cb3f0db7476509c09133d
Dany <danycambrano1@gmail.com>
2025-01-27 14:08:28 -0600

    Botones

--- src/components/ejemplos/Ejemplo01.js
+++ src/components/ejemplos/Ejemplo01.js
@@ -1,5 +1,9 @@
 import React, {useState} from 'react';
 
+import {Button} from "react-bootstrap";
+
+import "./Ejemplo01.scss";
+
 //hooks useState
 
 export  function Ejemplo01() {
@@ -8,11 +12,17 @@ export  function Ejemplo01() {
   const incrementarValor=()=>{
     setContador(contador + 1);
   }
-  
+  const derementarValor=()=>{
+    setContador(contador +-1);
+  }
+
   return (
-    <div>
+    <div className='container-celda'>
       <h1>Incremento: {contador}</h1>
-      <button onClick={incrementarValor}>Incrementar</button>
+      <div className='container-celda__button'>
+      <Button variant="primary" onClick={incrementarValor}><i class="bi bi-plus-circle"></i></Button>
+      <Button variant="primary" onClick={derementarValor}><i class="bi bi-dash-circle"></i></Button>
+      </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+//importar librerias css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import App from './App';
 
 
